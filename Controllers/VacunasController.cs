6a5a7518e64b111a95691702d52dbc1a5fb4cee5
Gonzalo Méndez <87814374+Gonzamen@users.noreply.github.com>
2021-07-22 10:40:08 -0300

    Controladores API

--- Controllers/VacunasController.cs
+++ Controllers/VacunasController.cs
@@ -1,114 +1,65 @@
-﻿using Dominio.Entidades;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Web;
-using System.Web.Mvc;
+using System.Web.Http;
+using Repositorios;
 
-namespace MVC.Controllers
+namespace WebAPI.Controllers
 {
-    public class VacunasController : Controller
+    [RoutePrefix("api/vacunas")]
+    public class VacunasController : ApiController
     {
-        private HttpClient clienteApi = new HttpClient();
-        private HttpResponseMessage respuesta = new HttpResponseMessage();
+        RepositorioVacuna repovacuna = new RepositorioVacuna();
 
-        private Uri UriBase = new Uri(@"http://localhost:50473/api/vacunas");
-
-        private void ConfigurarCliente()
-        {
-            clienteApi.BaseAddress = UriBase;
-
-            clienteApi.DefaultRequestHeaders.Accept.Clear();
-            clienteApi.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-        }
-
-        public ActionResult Filter()
-        {
-            if (Session["logueadocedula"] != null)
-            {
-                return View();
-            }
-            else
-            {
-                return RedirectToAction("Index", "Home");
-            }           
-        }
-
-        [HttpPost]
-        public ActionResult Filter(int? fase, int? rangomin, int? rangomax, string tipo, string laboratorio, string pais, string select)
+        [Route("filtradas")]
+        [HttpGet]
+        public IHttpActionResult FiltrarVacunas(int? fase, int? rangomin, int? rangomax, string tipo, string laboratorio, string pais)
         {
             try
             {
-                if ((rangomax == null && rangomin != null) || (rangomax != null && rangomin == null))
+                if(fase == null && rangomin == null && rangomax == null && tipo == null && laboratorio == null && pais == null)
                 {
-                    ViewBag.error = "Debe cargar ambos rangos de precio";
-                    return View();
-                }
-                if (select == "interseccion")
-                {
-                    ConfigurarCliente();
-                    respuesta = clienteApi.GetAsync(clienteApi.BaseAddress + "/" + "filtradas?fase=" + fase + "&rangomin=" + rangomin + "&rangomax=" + rangomax + "&tipo=" + tipo + "&laboratorio=" + laboratorio + "&pais=" + pais).Result;
-
-                    if (respuesta.IsSuccessStatusCode)
-                    {
-                        var contenido = respuesta.Content.ReadAsAsync<IEnumerable<Vacuna>>().Result;
-                        if (contenido != null)
-                        {
-                            ViewBag.lista = contenido;
-                        }
-                    }
-                    else
-                    {
-                        ViewBag.error = "No se obtuvo respuesta";
-                        return View();
-                    }
-                }
-                else if(select == "union")
-                {
-                    ConfigurarCliente();
-                    respuesta = clienteApi.GetAsync(clienteApi.BaseAddress + "/" + "filtradasunion?fase=" + fase + "&rangomin=" + rangomin + "&rangomax=" + rangomax + "&tipo=" + tipo + "&laboratorio=" + laboratorio + "&pais=" + pais).Result;
-
-                    if (respuesta.IsSuccessStatusCode)
-                    {
-                        var contenido = respuesta.Content.ReadAsAsync<IEnumerable<Vacuna>>().Result;
-                        if (contenido != null)
-                        {
-                            ViewBag.lista = contenido;
-                        }
-                    }
-                    else
-                    {
-                        ViewBag.error = "No se obtuvo respuesta";
-                        return View();
-                    }
+                    var vacunas = repovacuna.FindAll();
+                    return Ok(vacunas);
                 }
                 else
                 {
-                    ConfigurarCliente();
-                    respuesta = clienteApi.GetAsync(clienteApi.BaseAddress + "/" + "filtradas?fase=" + null + "&rangomin=" + null + "&rangomax=" + null + "&tipo=" + null + "&laboratorio=" + null + "&pais=" + null).Result;
-
-                    if (respuesta.IsSuccessStatusCode)
-                    {
-                        var contenido = respuesta.Content.ReadAsAsync<IEnumerable<Vacuna>>().Result;
-                        if (contenido != null)
-                        {
-                            ViewBag.lista = contenido;
-                        }
-                    }
-                    else
-                    {
-                        ViewBag.error = "No se obtuvo respuesta";
-                        return View();
-                    }
+                    var vacunas = repovacuna.FiltrarVacunas(fase, rangomin, rangomax, tipo, laboratorio, pais);
+                    return Ok(vacunas);
                 }
-                return View();
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                ModelState.AddModelError("Se produjo una excepción: ", ex.Message);
-                return View();
+                return InternalServerError
+                (new Exception("No es posible filtrar las vacunas",
+                ex.InnerException));
+            }
+        }
+
+        [Route("filtradasunion")]
+        [HttpGet]
+        public IHttpActionResult FiltrarVacunasUnion(int? fase, int? rangomin, int? rangomax, string tipo, string laboratorio, string pais)
+        {
+            try
+            {
+                if (fase == null && rangomin == null && rangomax == null && tipo == null && laboratorio == null && pais == null)
+                {
+                    var vacunas = repovacuna.FindAll();
+                    return Ok(vacunas);
+                }
+                else
+                {
+                    var vacunas = repovacuna.FiltrarVacunasUnion(fase, rangomin, rangomax, tipo, laboratorio, pais);
+                    return Ok(vacunas);
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError
+                (new Exception("No es posible filtrar las vacunas",
+                ex.InnerException));
             }
         }
     }
